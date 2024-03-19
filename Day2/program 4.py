62fd6cd2e88a58483601c9e4c7f7c9fc650e1b16
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-03-19 13:09:45 +0530

    Create program 4.py

---
+++ Day2/program 4.py
@@ -0,0 +1,8 @@
+def merge_list(lst1,lst2):
+    merged_lst= lst1 + lst2
+    return merged_lst
+
+lst1=[1,2,3]
+lst2=[4,5,6]
+merged_sort=merge_list(lst1,lst2)
+print("the mergerd list:",merged_sort)
