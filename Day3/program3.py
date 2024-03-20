93cba2cf8f564eff7404006a956355cde3f0b6cc
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-03-20 12:33:53 +0530

    Create program3.py

---
+++ Day3/program3.py
@@ -0,0 +1,9 @@
+def num_good_pairs(nums):
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                count += 1
+    return count
+nums = [1, 2, 3, 1, 1, 3]
+print(num_good_pairs(nums)) 
