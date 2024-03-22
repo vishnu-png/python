3b7267ff58fbc46cdc90270e61c9f56651e8baa4
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-03-22 10:42:31 +0530

    Create program3.py

---
+++ Day5/program3.py
@@ -0,0 +1,13 @@
+def numSquares(n):
+    dp = [0] * (n + 1)
+    
+    for i in range(1, n + 1):
+        dp[i] = i  
+        j = 1
+        while j * j <= i:
+            dp[i] = min(dp[i], dp[i - j * j] + 1)
+            j += 1
+    
+    return dp[n]
+n = 12
+print(numSquares(n))  
