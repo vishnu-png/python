b1eab7b030a01e6ef976fe9b8a8a02a7f6faf18f
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-03-22 10:45:09 +0530

    Create program5.py

---
+++ Day5/program5.py
@@ -0,0 +1,15 @@
+def findPeakElement(nums):
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        mid = left + (right - left) // 2
+
+        if nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return left
+
+nums = [1, 2, 3, 1]
+print(findPeakElement(nums)) 
